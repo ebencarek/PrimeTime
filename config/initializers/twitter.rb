97ffbb5fe8bcb3168361f1865c8765585cce1461
Eben Carek <ebengc@gmail.com>
2017-04-01 01:51:51 -0400

    initialized project, wrote main script

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,21 @@
+require 'prime'
+
+CLIENT = Twitter::REST::Client.new do |config|
+  config.consumer_key = ENV['consumer_key']
+  config.consumer_secret = ENV['consumer_secret']
+  config.access_token = ENV['access_token']
+  config.access_token_secret = ENV['access_secret']
+end
+
+Thread.new do
+  while true
+    # only run once per minute, won't check seconds of the time
+    sleep 60
+    time = Time.now
+    time_num = time.strftime("%H%M").to_i
+    # tweet if the current time is a prime number
+    if Prime.prime?(time_num)
+      CLIENT.update("The current time is #{time.strftime('%H:%M')}.\nIT'S PRIME TIME.")
+    end
+  end
+end
